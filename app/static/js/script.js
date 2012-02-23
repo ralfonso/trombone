b035568c9bfdae6be9e6cc6861fd1afc0e35824a
Nichole Barrett <elohcin@gmail.com>
2012-02-23 09:45:01 -0800

    indents, etc.

--- app/static/js/script.js
+++ app/static/js/script.js
@@ -18,48 +18,47 @@ Master Javascript
 ----------------------------------------------------------------------*/
 
 var TROMBONE = {
+	
+	server: function(method, type, data, success) {
+		$.ajax({
+			type: type,
+			url: '/extensions/'+ method,
+			dataType: 'json',
+            data: data,
+            success: function(result) {
+            	if (success) success(result);  
+            }
+        });
+    },
   
-  server: function(method, type, data, success) {
-      $.ajax({
-          type: type,
-          url: '/extensions/'+ method,
-          dataType: 'json',
-          data: data,
-          success: function(result) {
-              if (success) success(result);  
-          }
-      });
-  },
-  
-  common: {
+  	common: {
+    	init: function(){
+    	}
         
-    init: function(){
-    }
-        
-  },
+  	},
     
   /*- Login Page Methods
   ------------------------------------------------------------------*/
-  index: {  	
-  	init: function() {
-     $.ajax({
-        url:  '/api/user/list/top',
-        type: 'GET',
-        data: {'as_html' : true},
-        dataType: 'html',
-        success: function (data, status) {
-            $('#top-scores-container').html(data);
-            $('#top-scores li a').click(TROMBONE.index.selectUser);
-        },
-        error: function (errors) {
+  	index: {  	
+  		init: function() {
+     		$.ajax({
+        		url:  '/api/user/list/top',
+        		type: 'GET',
+        		data: {'as_html' : true},
+        		dataType: 'html',
+        		success: function (data, status) {
+            		$('#top-scores-container').html(data);
+            		$('#top-scores li a').click(TROMBONE.index.selectUser);
+        		},
+        		error: function (errors) {
 
-        },
-        complete: function () {
+        		},
+        		complete: function () {
 
-        }
-     });
+        		}
+     		});
      
-      function validation(form) {
+      		function validation(form) {
     			var valid = true;
 
     			// check if required fields are blank
@@ -71,11 +70,11 @@ var TROMBONE = {
   						$(el).removeClass('error');
   					}
     			});
-
+    		
     			return valid;
     		}
 
-    	function submitForm (e) {
+			function submitForm (e) {
     			var form = $(this);
 
     			e.preventDefault();
@@ -90,13 +89,13 @@ var TROMBONE = {
     					data: form.serialize(),
     					success: function (data, status) {
     						if ( data.success ) {
-    						  form.removeClass('show-errors').addClass('ok');
-      						  window.location="/demerit";
+    							form.removeClass('show-errors').addClass('ok');
+      							window.location="/demerit";
     						}
     						else {
-    						  $('#errors').append('<p>'+data.message+'</p>'); 
-      						form.addClass('show-errors');
-      						$('#errors').show();
+    							$('#errors').append('<p>'+data.message+'</p>'); 
+      							form.addClass('show-errors');
+      							$('#errors').show();
     						}
     					},
     					error: function (error) {
@@ -109,201 +108,205 @@ var TROMBONE = {
     					}
     				});
     			}
-    		}
+			}
     		
     		$('form').submit( submitForm );
-    },
+    	},
 
-    selectUser: function(e) {
-      var slug = e.target.getAttribute('data-user-slug')
-      $('#top-scores li a').css('color', '');
-      $(e.target).css('color', '#F38630');
-      $('#to-user').val(slug);
-      $('#reasons').load('/api/demerit/list/' + slug + '?as_html=true');
-    }
+    	selectUser: function(e) {
+    		var slug = e.target.getAttribute('data-user-slug')
+      		$('#top-scores li a').css('color', '');
+      		$(e.target).css('color', '#F38630');
+      		$('#to-user').val(slug);
+      		$('#reasons').load('/api/demerit/list/' + slug + '?as_html=true');
+    	}
 
-  },
+  	},
   
   /*- Sign Up Page Methods
   ------------------------------------------------------------------*/
-  demerit: {
-    init: function(){
-      var to_user = $('#to-user');
-      var form = $('#reason');
+	demerit: {
+		init: function(){
+      		var to_user = $('#to-user');
+      		var form = $('#reason');
       
-      console.log(form);
+      		console.log(form);
 
-      $.ajax({
-        url:  '/api/user/list',
-        type: 'GET',
-        dataType: 'json',
-        data: form.serialize(),
-        success: function (data, status) {
-            $.each(data.users, function(index, user) {
-                console.log(user);
-                var option = $('<option/>');
-                option.attr('value', user.slug);
-                option.text(user.first_name + ' ' + user.last_name);
-                to_user.append(option);
-            });
-        },
-        error: function (errors) {
+      		$.ajax({
+        		url:  '/api/user/list',
+        		type: 'GET',
+        		dataType: 'json',
+        		data: form.serialize(),
+        		success: function (data, status) {
+            		$.each(data.users, function(index, user) {
+                	console.log(user);
+                	var option = $('<option/>');
+                	option.attr('value', user.slug);
+                	option.text(user.first_name + ' ' + user.last_name);
+                	to_user.append(option);
+            		});
+        		},
+        		error: function (errors) {
 
-        },
-        complete: function () {
+        		},
+        		complete: function () {
 
-        }
-     });
+        		}
+     		});
 
-     function updateTopScores() {
-         $.ajax({
-            url:  '/api/user/list/top',
-            type: 'GET',
-            dataType: 'html',
-            data: form.serialize(),
-            success: function (response, status) {
-                $('#top-scores-container').html(response);
-                $('#top-scores li a').click(TROMBONE.demerit.selectUser);
-            },
-            error: function (errors) {
+     		function updateTopScores() {
+         		$.ajax({
+            		url:  '/api/user/list/top',
+            		type: 'GET',
+            		dataType: 'html',
+            		data: form.serialize(),
+            		success: function (response, status) {
+                		$('#top-scores-container').html(response);
+                		$('#top-scores li a').click(TROMBONE.demerit.selectUser);
+            		},
+            		error: function (errors) {
 
-            },
-            complete: function () {
+            		},
+            		complete: function () {
 
-            }
-         });
-     }
+            		}
+         		});
+     		}
       
-      function validation(form) {
-    		var valid = true;
+      		function validation(form) {
+    			var valid = true;
 
-  			//  check if required fields are blank
-  			$(form).find('input.required').each( function (i, el) {
-  				if (!el.value) {
+  				//  check if required fields are blank
+  				$(form).find('input.required').each( function (i, el) {
+  					if (!el.value) {
 						valid = false;
 						$(el).addClass('error');
 					} else {
 						$(el).removeClass('error');
 					}
-  			});
-
-  			return valid;
-  		}
-
-    	function submitForm (e) {
-    	  var form = $(this);
-
-  			e.preventDefault();
-
-  			if (validation(form)) {
-            var form_data = form.serialize();
-            if ($('#to-user').val() == '')
-              return;
-
-  			$.ajax({
-  			  url: form.attr('action'),
-  			  type: 'POST',
-  			  data: form_data,
-  			  success: function (data, status) {
-  				if ( data.success ) {
-                  $('input[name=reason]').val('');
-                  var message = $('#message');
-                  var user = data.user;
-                  var noun = user.demerits == 1 ? 'demerit' : 'demerits';
-                  message.text(user.first_name + ' ' + user.last_name + ' now has ' + user.demerits + ' ' + noun + '.');
-                  updateTopScores();
-                  $('#reasons').load('/api/demerit/list/' + user.slug + '?as_html=true');
-  						}
-  						else {
-                  var message = $('#message');
-                  message.text(data.message);
-  						}
-  					},
-  					error: function (error) {
-  					},
-  					complete: function () {
-  					}
   				});
-  			}
-  		}
 
-  		$('#reason').submit( submitForm );
-        updateTopScores();
-    },
-    selectUser: function(e) {
-      var slug = e.target.getAttribute('data-user-slug')
-      $('#to-user').val(slug);
-      $('#reasons').load('/api/demerit/list/' + slug + '?as_html=true');
-    },
+  				return valid;
+  			}
+
+    		function submitForm (e) {
+    	  		var form = $(this);
+
+  				e.preventDefault();
+
+  				if (validation(form)) {
+            		var form_data = form.serialize();
+            		if ($('#to-user').val() == '')
+              		return;
+
+  					$.ajax({
+  			  			url: form.attr('action'),
+  			  			type: 'POST',
+  			  			data: form_data,
+  			  			success: function (data, status) {
+  							if ( data.success ) {
+                  				$('input[name=reason]').val('');
+                  				var message = $('#message');
+                  				var user = data.user;
+                  				var noun = user.demerits == 1 ? 'demerit' : 'demerits';
+                  				message.text(user.first_name + ' ' + user.last_name + ' now has ' + user.demerits + ' ' + noun + '.');
+                  				updateTopScores();
+                  				$('#reasons').load('/api/demerit/list/' + user.slug + '?as_html=true');
+  							}
+  							else {
+                  				var message = $('#message');
+                  				message.text(data.message);
+  							}
+  						},
+  						error: function (error) {
+  						},
+  						complete: function () {
+  						}
+  					});
+  				}
+  			}
+
+  			$('#reason').submit( submitForm );
+        	updateTopScores();
+    	},
+    	selectUser: function(e) {
+      		var slug = e.target.getAttribute('data-user-slug')
+      		$('#to-user').val(slug);
+      		$('#reasons').load('/api/demerit/list/' + slug + '?as_html=true');
+    	},
     
-    /* when click on demerit, show excuse or ze form */
-  	openDemeritForm: function(demerit_id) {
-  		
-   	  $('#demerit-' + demerit_id).load('/api/excuse/list/' + demerit_id + '?as_html=true'/*, function (){ toggle }*/);   	 
-   }
+    	/* when click on demerit, show excuse or ze form */
+  		openDemeritForm: function(demerit_id) {
+   	  		$('#demerit-' + demerit_id).load('/api/excuse/list/' + demerit_id + '?as_html=true'/*, function (){ toggle }*/); 
+   	  	TROMBONE.excuse.init();  	 
+   		}
    	
-  },
+  	},
 
 
   /*- EXCUSES
   ----------------------------------------------------------------------*/
-  excuse: {
-    init: function () {
-      var form = $('#excuses');
+  	excuse: {
+    	init: function () {
+      		var form = $('#excuseForm');
+      		console.log('we are in excuse init!');
 
-        function validation(form) {
-            var valid = true;
+        	function validation(form) {
+            	var valid = true;
             
-            $(form).find('input.required').each ( function (i, el) {
-                if (!el.value){
-                    valid = false;
-                    $(el).addClass('error');
-                } else {
-                    $(el).removeClass('error');    
-                }    
-            });
+            	$(form).find('input.required').each ( function (i, el) {
+                	if (!el.value){
+                    	valid = false;
+                    	$(el).addClass('error');
+                	} else {
+                    	$(el).removeClass('error');    
+               	 	}    
+            	});
 
-  		    return valid;
-        }
+  		    	return valid;
+        	}
       
-        function submitForm(e) {  
-          var form = $(this); 
+        	function submitForm(e) {  
+          		var form = $(this); 
                 
-            e.preventDefault();
+            	e.preventDefault();
         
-            if (validation(form)) {
-            var form_data = form.serialize();
-                return;
-
-            $.ajax({
-                url: form.attr('action'),
-                type: 'POST',
-                data: form_data,
-                success: function (data, status) {
-                    if ( data.success ) {
-                        $('input[name=excuse]').val('');
-                        $('#excuses').load('/api/excuse/list/' + demerit_id + '?as_html=true');
-				    }
-				    else { 
-                 var message = $('#message');
-                 message.text(data.message);
-				    }
-			     },
-			     error: function (error) {
-			     },
-			     complete: function () {
-			     }
-            });
-            } 
-        }
-        $('#excuses').submit( submitForm );	  
-    }, 
-    addExcuse: function(e) {
-        var demerit_id = e.target.getAttribute('data-excuse-to_demerit_id');
-        $('#excuse').load('/api/excuse/list/' + demerit_id + '?as_html=true');
-    }
+            	if (validation(form)) {
+            		var form_data = form.serialize();
+                
+              		$.ajax({
+                		url: form.attr('action'),
+                		type: 'POST',
+                		data: form_data,
+                		success: function (data, status) {
+                    		if ( data.success ) {
+                      			$('input[name=excuse]').val('');
+                	  			var demerit_id = $('input[name=demerit_id]').val('');
+                	  			console.log(demerit_id);
+                	  			$('#excuses').load('/api/excuse/list/' + demerit_id + '?as_html=true');
+				    		}
+				    		else { 
+                 				var message = $('#message');
+                 				message.text(data.message);
+				    		}
+			     		},
+			     		error: function (error) {
+			     		},
+			     		complete: function () {
+			     		}
+              		});
+            	} 
+        	}
+        	$('#excuseForm').submit( submitForm );	  
+    	}, 
+    	addExcuse: function(e) {
+    		var demerit_id = $('input[name=demerit_id]').val('');
+    		//var demerit_id = data.demerit.id
+    		console.log(demerit_id);
+        	$('#excuses').load('/api/excuse/list/' + demerit_id + '?as_html=true');
+    	}
     
-  }
+  	}
 
 }; /* end of TROMBONE */ 
 
@@ -313,24 +316,24 @@ var TROMBONE = {
 
 var UTIL = {
   
-  exec: function( controller, action ) {
-    var ns = TROMBONE;
+	exec: function( controller, action ) {
+    	var ns = TROMBONE;
       
-    action = ( action === undefined ) ? "init" : action;
+    	action = ( action === undefined ) ? "init" : action;
         
-    if ( controller !== "" && ns[controller] && typeof ns[controller][action] == "function" ) {
-        ns[controller][action]();
-    }
-  },
+    	if ( controller !== "" && ns[controller] && typeof ns[controller][action] == "function" ) {
+        	ns[controller][action]();
+    	}
+  	},
     
-  init: function() {
-    var body = document.body,
+  	init: function() {
+    	var body = document.body,
         controller = body.getAttribute( "data-controller" ),
         action = body.getAttribute( "data-action" );
         
-    UTIL.exec( "common" );
-    UTIL.exec( action );
-  }  
+    	UTIL.exec( "common" );
+    	UTIL.exec( action );
+  	}  
 
 };
 
